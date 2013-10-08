8d4030bcdfc47f5cbd9009ca49e51a33cdbb4584
David Gobbi <david.gobbi@gmail.com>
2013-10-08 12:17:13 -0600

    Rename files to end in .cxx instead of .cc.

--- test/main.cxx
+++ test/main.cxx
@@ -1,7 +1,7 @@
 /*
  * This file includes the magic necessary in order to get your unit tests
  * to automatically run. There should never be a reason to modify this file.
- * For an example unit test, see "sanity_check.cpp".
+ * For an example unit test, see "sanity_check.cxx".
  */
 
 #include "test.h"
